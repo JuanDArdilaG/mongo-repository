5829c80fb3378ebb72c7be37a944afc5f99102a9
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2023-09-17 18:06:52 -0500

    [Add] getBy method

--- src/MongoRepository.ts
+++ src/MongoRepository.ts
@@ -24,6 +24,14 @@ export abstract class MongoRepository<T extends AggregateRoot>
     }) as T[];
   }
 
+  async getBy(key: string, value: any): Promise<T[]> {
+    const collection = await this.collection();
+    const documents = await collection.find({ [key]: value }).toArray();
+    return documents.map((prim) => {
+      return this._example.fromPrimitives(prim);
+    }) as T[];
+  }
+
   async getByID(id: Identifier<string>): Promise<T> {
     const collection = await this.collection();
     const document = await collection.findOne({
